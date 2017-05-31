dc27bee1197f81eaaf46ab3b50e6d1a767a0316a
NK <natthaphol@gopomelo.com>
2017-05-31 17:37:23 +0700

    initial

---
+++ go/default.go
@@ -0,0 +1,30 @@
+package 
+
+import (
+	"net/http"
+)
+
+type Default struct {
+
+}
+
+func AddPet(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func DeletePet(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func FindPetById(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func FindPets(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
